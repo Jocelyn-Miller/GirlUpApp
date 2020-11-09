20cd7b375335aaed2d1134247fe4dcde7e02e629
Jocelyn <joeymmiller@gmail.com>
2020-11-09 09:16:47 -0500

    Initial Commit

---
+++ Girl Up APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Girl Up APP
+//
+//  Created by Jocelyn Miller on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
