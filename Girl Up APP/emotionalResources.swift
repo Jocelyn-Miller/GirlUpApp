c74f34fb3d431bf3aaac05bffbe390a79148e9b7
Jocelyn <joeymmiller@gmail.com>
2021-01-26 18:51:40 -0500

    updated emotional resources

--- Girl Up APP/emotionalResources.swift
+++ Girl Up APP/emotionalResources.swift
@@ -12,66 +12,53 @@ class emotionalResources: UIViewController, MFMailComposeViewControllerDelegate
 {
     
 var phoneNumber = 4849038439
+    var crisisSupportNum = 800273835
+    var crisisSupportNumText = 839863
+    var hotlineNum = 18006564673
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        // guidance emails
-        
-        // aalderfer@haverfordsd.net
-        // ccostanzo@haverfordsd.net
-        // pdayl@haverfordsd.net
-        //kjones@haverfordsd.net
-        //lpellicane@haverfordsd.net
-        //kvindler@haverfordsd.net
-        //mwilliams@haverfordsd.net
 
-        //therapy options
-        //24/7 crisis hotline
-        
-        //sexual assualt hotline
-        // 1-800-656-4673
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func textCrisis(_ sender: UIButton)
+    {
+        print("text crisis")
+        if let textURL = URL(string: "sms://\(crisisSupportNumText)") {
+        
+        let application:UIApplication = UIApplication.shared
+        if (application.canOpenURL(textURL)) {
+            application.open(textURL, options: [:], completionHandler: nil)
+        }
+      }
     }
     
-
-    @IBAction func callCell(_ sender: UIButton)
+    @IBAction func callCrisis(_ sender: UIButton)
     {
-        if let phoneCallURL = URL(string: "tel://\(phoneNumber)") {
+        print("call crisis")
+        if let phoneCallURL = URL(string: "tel://\(crisisSupportNum)") {
         
         let application:UIApplication = UIApplication.shared
         if (application.canOpenURL(phoneCallURL)) {
             application.open(phoneCallURL, options: [:], completionHandler: nil)
         }
       }
-        
     }
     
-    @IBAction func sendMail(_ sender: UIButton)
+    @IBAction func callHotline(_ sender: UIButton)
     {
-            if MFMailComposeViewController.canSendMail()
-            {
-                let mail = MFMailComposeViewController()
-                mail.mailComposeDelegate = self
-                mail.setToRecipients(["joeymmiller@gmail.com"])
-                mail.setMessageBody("<p>Test Complete!</p>", isHTML: true)
+        print("call hotline")
 
-                present(mail, animated: true)
-            } else {
-                let alertWin = UIAlertController(title: "email send failed", message: nil, preferredStyle: .alert)
-               alertWin.addAction(UIAlertAction(title: NSLocalizedString("fork", comment: "Default action"), style: .default, handler: { _ in
-               NSLog("The \"OK\" alert occured.")
-               }))
-                self.present(alertWin, animated: true, completion: nil)
-            }
+        if let phoneCallURL = URL(string: "tel://\(hotlineNum)") {
+        
+        let application:UIApplication = UIApplication.shared
+        if (application.canOpenURL(phoneCallURL)) {
+            application.open(phoneCallURL, options: [:], completionHandler: nil)
         }
-
-        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?)
-        {
-            controller.dismiss(animated: true)
-        }
+      }
+    }
     
     
 }
